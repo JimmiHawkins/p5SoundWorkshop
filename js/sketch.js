fd7fce7d2b2c2643e822c2a690e9da62bb6cf7a2
JimmiHawkins <79973489+JimmiHawkins@users.noreply.github.com>
2021-11-08 16:39:12 +0000

    First visual

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,38 @@
+let mySong, amplitude;
+
+function preload(){
+    mySong = loadSound('assets/massiveAttackAngel.mp3')
+
+}
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
+    
+    mySong.play();
+    
+    amplitude = new p5.Amplitude();
 }
 
 function draw() {
-    background(220);
+    let level = amplitude.getLevel();
+    let col = map(level,0,1,0,255);
+    let shape
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    console.log(col);
+    
+    background(col,0,0);
+    fill(255,50,50);
+
+    if (col > 40) {
+        fill(255,50,50);
+        shape = ellipse(width/2, height/2, col*3);
+        ellipse(200, height/2, col*3);
+        ellipse(800, height/2, col*3);
+        
+    } else {
+        fill(255,255,255)
+        shape = ellipse(width/2, height/2, col*3);
+    }
+    
 }
