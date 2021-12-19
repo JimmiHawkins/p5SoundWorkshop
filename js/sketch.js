9356f91ff8c9c4866f22c6cb121168791f43443b
JimmiHawkins <79973489+JimmiHawkins@users.noreply.github.com>
2021-12-19 13:30:34 +0000

    Fear

--- js/sketch.js
+++ js/sketch.js
@@ -1,38 +1,51 @@
-let mySong, amplitude;
+var mySong, amplitude, play;
 
 function preload(){
-    mySong = loadSound('assets/massiveAttackAngel.mp3')
-
+    mySong = loadSound('assets/dark.mp3');
 }
 
 function setup() {
     var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
-    
+
     mySong.play();
     
     amplitude = new p5.Amplitude();
-}
+    background(0);
+
+    };
 
 function draw() {
-    let level = amplitude.getLevel();
-    let col = map(level,0,1,0,255);
-    let shape
+ 
+    var level = amplitude.getLevel();
+    var col = map(level, 0, 1, 0, 255);
 
-    console.log(col);
-    
-    background(col,0,0);
-    fill(255,50,50);
+    background(0,0,0);
+    noStroke(0);
 
-    if (col > 40) {
-        fill(255,50,50);
-        shape = ellipse(width/2, height/2, col*3);
-        ellipse(200, height/2, col*3);
-        ellipse(800, height/2, col*3);
+    fill(255,255,255);    
+
+        ellipse(width/2, height/1.9, 300, col*3 );
+        ellipse(width-1000, height/1.5, 300, col*3);
+        ellipse(width-1560, height/1.5, 300, col*3);
+        ellipse(width-1000, height/2.5, 300, col*3);
+        ellipse(width-1560, height/2.5, 300, col*3);
+
         
-    } else {
-        fill(255,255,255)
-        shape = ellipse(width/2, height/2, col*3);
-    }
-    
+        fill(col*3,50,60);
+
+        ellipse(width/2, height/1.9, 250, col*3/1.3 );
+        ellipse(width-1000, height/1.5, 250, col*3/1.3);
+        ellipse(width-1560, height/1.5, 250, col*3/1.3);
+        ellipse(width-1000, height/2.5, 250, col*3/1.3);
+        ellipse(width-1560, height/2.5, 250, col*3/1.3);
+
+        fill(0,0,0);
+        
+        ellipse(width/2, height/1.9, col*3/1.3 );
+        ellipse(width-1000, height/1.5, col*3/1.3);
+        ellipse(width-1560, height/1.5, col*3/1.3);
+        ellipse(width-1000, height/2.5, col*3/1.3);
+        ellipse(width-1560, height/2.5, col*3/1.3);
+   
 }
